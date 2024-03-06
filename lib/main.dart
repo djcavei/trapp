82c5c85c2d2b51e2b9a31cb560a870b79e6c8c8d
Ricky <martignago2@gmail.com>
2024-03-06 21:11:53 +0100

    const AppRules object

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   static final List<Widget> _widgets = <Widget>[
     AppStats(key: GlobalKey()),
-    AppRules()
+    const AppRules()
   ];
 
   int _selectedIndex = 0;
